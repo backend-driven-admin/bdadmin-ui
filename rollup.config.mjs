a343151f7ac197cc62f00e5bda8bf9f23c6a5bd9
ShotMeow <shotmeow@gmail.com>
2025-02-17 14:47:02 +0300

    Init: First commit

---
+++ rollup.config.mjs
@@ -0,0 +1,27 @@
+
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import { defineConfig } from "rollup";
+import terser from "@rollup/plugin-terser";
+import json from "@rollup/plugin-json";
+
+const packageJson = require("./package.json");
+
+export default defineConfig([
+    {
+        input: "src/index.ts",
+        output: {
+            file: packageJson.main,
+            format: "cjs",
+            banner: "#!/usr/bin/env node",
+        },
+        plugins: [
+            json(),
+            resolve(),
+            commonjs(),
+            typescript(),
+            terser(),
+        ],
+    },
+]);
